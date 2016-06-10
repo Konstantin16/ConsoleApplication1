3c5ed3e2cf02c2f280f096f55ec9ad43d0991090
Усс.Константин <.@PC35-12-P>
2016-06-10 18:30:58 +0300

    18:30

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ В структуре не может быть конструктора по умолчанию
+     */
+
 namespace ConsoleApplication1
 {
     class Program
